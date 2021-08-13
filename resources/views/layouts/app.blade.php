acb9502237b755b4670cc26cb0a88d4a7678f9e7
William <work@suppo.fr>
2021-08-13 13:45:41 +0200

    Un Ladder peut avoir une miniature.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,13 +14,13 @@
         <!-- Styles -->
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
-    <body class="font-sans antialiased bg-charcoal">
+    <body class="font-sans antialiased bg-secondary">
         <div id="app">
             <div class="min-h-screen">
 
                 <!-- Page Heading -->
-                <header class="flex justify-between flex-wrap bg-prussian-blue">
-                    <div class="text-3xl text-portland-orange ml-4 my-2 md:mt-0 md:my-0 md:mx-4 md:w-auto md:text-left self-center font-title">Versus</div>
+                <header class="flex justify-between flex-wrap bg-secondary-dark">
+                    <div class="text-3xl text-primary ml-4 my-2 md:mt-0 md:my-0 md:mx-4 md:w-auto md:text-left self-center font-logo">Versus</div>
 
                     <div class="block self-center mr-4 md:hidden">
                         <button id="buttonMenu" class="flex items-center px-3 py-2 text-white focus:outline-none" onclick="toggleMenu()">
@@ -32,14 +32,14 @@
 
                     <nav id="navMenu" class="w-full block flex-grow text-white mt-2 md:mt-0 md:flex md:items-center md:w-auto hidden">
                         <div class="md:flex-grow">
-                            <a href="{{ route('ladder.index') }}" class="block py-4 pl-4 flex items-center md:inline-block md:mt-0 hover:text-portland-orange">
+                            <a href="{{ route('ladder.index') }}" class="block py-4 pl-4 flex items-center md:inline-block md:mt-0 hover:text-primary">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-3 md:hidden" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16V4m0 0L3 8m4-4l4 4m6 0v12m0 0l4-4m-4 4l-4-4" />
                                 </svg>
                                 Jouer
                             </a>
                             @if(Auth::check() && Auth::user()->isAdmin())
-                                <a href="#" class="block py-4 pl-4 flex items-center md:inline-block md:mt-0 hover:text-portland-orange">
+                                <a href="#" class="block py-4 pl-4 flex items-center md:inline-block md:mt-0 hover:text-primary">
                                     <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-3 md:hidden" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
@@ -50,7 +50,7 @@
                         </div>
                         <div>
                             @guest
-                                <a href="{{ route('login') }}" class="m-4 px-4 py-2 text-center block text-sm border-portland-orange border rounded text-portland-orange hover:border-white hover:text-white">
+                                <a href="{{ route('login') }}" class="m-4 px-4 py-2 text-center block text-sm border-primary border rounded text-primary hover:border-white hover:text-white">
                                     Se connecter
                                 </a>
                             @endguest
