4a33bc6263c7c6cf03cb0b993d5096d9a5b042f9
William <work@suppo.fr>
2021-08-14 00:03:29 +0200

    Petits ajustements du design.

--- resources/views/ladder/index.blade.php
+++ resources/views/ladder/index.blade.php
@@ -4,32 +4,36 @@
     <h1 class="font-bold text-3xl text-white mt-8 mb-4 md:w-2/3 md:mx-auto">Ladders</h1>
     <div class="grid grid-cols-1 md:grid-cols-4 gap-4 mb-4 md:w-2/3 md:mx-auto">
         @foreach($ladders as $ladder)
-            <div class="border-prussian-blue bg-secondary-light hover:bg-secondary-dark">
-                <a href="{{ route('ladder.ranking', $ladder) }}">
-                    <div class="h-32 bg-cover bg-center" style="background-image: url({{ route('image', $ladder->thumbnail) }})">&nbsp;</div>
-                    <div class="px-6 py-4">
-                        <div class="font-bold text-xl text-primary mb-2">
-                            {{ $ladder->name }}
-                        </div>
-                        <p class="text-white text-sm text-shadow">
-                            {{ $ladder->description }}
-                        </p>
-                        <ul class="text-gray-300 text-sm mt-4">
-                            <li class="flex items-center">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
-                                </svg>
-                                {{ $ladder->teams_count }} participants
-                            </li>
-                            <li class="flex items-center mt-2">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
-                                </svg>
-                                Créé le {{ $ladder->created_at->format('d/m/Y') }}
-                            </li>
-                        </ul>
+            <div class="bg-secondary-light flex flex-col">
+                <div class="h-32 bg-cover bg-center" style="background-image: url({{ route('image', $ladder->thumbnail) }})">&nbsp;</div>
+                <div class="flex-grow px-6 py-4">
+                    <div class="font-bold text-xl text-primary mb-2">
+                        {{ $ladder->name }}
                     </div>
-                </a>
+                    <p class="text-white text-sm text-shadow">
+                        {{ $ladder->description }}
+                    </p>
+                    <ul class="text-gray-300 text-sm mt-4">
+                        <li class="flex items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
+                            </svg>
+                            {{ $ladder->teams_count }} participants
+                        </li>
+                        <li class="flex items-center mt-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+                            </svg>
+                            Créé le {{ $ladder->created_at->format('d/m/Y') }}
+                        </li>
+                    </ul>
+                </div>
+                <div class="flex items-stretch justify-between mt-4">
+                    @role(['admin'])
+                        <x-link-secondary href="#" class="p-3 m-4 w-full">Editer</x-link-secondary>
+                    @endrole
+                    <x-link href="{{ route('ladder.ranking', $ladder) }}" class="p-3 m-4 w-full">Voir</x-link>
+                </div>
             </div>
         @endforeach
     </div>
