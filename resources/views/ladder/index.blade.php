55662f38d12e86422df841d6ddf8ed149d83700f
William <work@suppo.fr>
2021-08-14 00:11:51 +0200

    Lien vers l'interface de création.

--- resources/views/ladder/index.blade.php
+++ resources/views/ladder/index.blade.php
@@ -1,7 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1 class="font-bold text-3xl text-white mt-8 mb-4 md:w-2/3 md:mx-auto">Ladders</h1>
+    <h1 class="font-bold text-3xl text-white mt-8 mb-6 md:w-2/3 md:mx-auto md:flex md:items-center">
+        <span class="md:flex-grow">
+            Ladders
+        </span>
+        @role(['admin'])
+            <x-link href="{{ route('ladder.create') }}" class="hidden md:block p-3">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4" />
+                </svg>
+            </x-link>
+        @endrole
+    </h1>
+    @role(['admin'])
+        <x-link href="{{ route('ladder.create') }}" class="md:hidden fixed bottom-0 right-0 p-3 mr-4 mb-4">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4" />
+            </svg>
+        </x-link>
+    @endrole
     <div class="grid grid-cols-1 md:grid-cols-4 gap-4 mb-4 md:w-2/3 md:mx-auto">
         @foreach($ladders as $ladder)
             <div class="bg-secondary-light flex flex-col">
@@ -30,7 +48,7 @@
                 </div>
                 <div class="flex items-stretch justify-between mt-4">
                     @role(['admin'])
-                        <x-link-secondary href="#" class="p-3 m-4 w-full">Editer</x-link-secondary>
+                        <x-link-secondary href="{{ route('ladder.edit', $ladder) }}" class="p-3 m-4 w-full">Editer</x-link-secondary>
                     @endrole
                     <x-link href="{{ route('ladder.ranking', $ladder) }}" class="p-3 m-4 w-full">Voir</x-link>
                 </div>
