dfd27c99b1c66f2c023975f6a4f51fbcb4e7a0c9
William <work@suppo.fr>
2021-08-18 19:23:05 +0200

    Correction de l'affichage du bouton de suppression.

--- resources/views/game/index.blade.php
+++ resources/views/game/index.blade.php
@@ -33,11 +33,11 @@
                         <td class="py-4 px-2 text-left">{{ $game->teams->last()->name }}</td>
                         <td class="py-4 px-4 text-left flex items-center justify-end">
                             @can('delete', $game)
-                                <form method="POST" action="{{ route('game.destroy', [$ladder, $game]) }}">
+                                <form method="POST" action="{{ route('game.destroy', [$ladder, $game]) }}" class="flex">
                                     @method('delete')
                                     @csrf
 
-                                    <x-link href="{{ route('game.destroy', [$ladder, $game]) }}" onclick="event.preventDefault(); this.closest('form').submit();" class="p-2">
+                                    <x-link href="{{ route('game.destroy', [$ladder, $game]) }}" onclick="event.preventDefault(); this.closest('form').submit();" class="p-2 inline-block">
                                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
                                         </svg>
